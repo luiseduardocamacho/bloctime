985d5cb6e1f538c86270afff5c870b5f79f4a915
Luis Camacho <luiscamacho@gmail.com>
2017-11-23 14:08:05 -0500

    Added On Break functionality

--- app/scripts/controllers/LandingCtrl.js
+++ app/scripts/controllers/LandingCtrl.js
@@ -1,16 +1,20 @@
 (function(){
   function LandingCtrl($interval){
     var $ctrl = this;
-    $ctrl.time = 1500;
+    $ctrl.time = 10;
     $ctrl.StartPauseTime = StartPauseTime;
     $ctrl.StartTime = StartTime;
     $ctrl.state = 'Start';
     $ctrl.RestartTimer = RestartTimer;
+    $ctrl.onBreak = false;
+    $ctrl.onBreakLabel = 'On Break';
+    $ctrl.onBreakRestartLabel = 'On Break - Click to Start New Work Session'
 
     function StartPauseTime(){
       if ($ctrl.state == 'Start'){
         StartTime();
       }
+
       else if($ctrl.state == 'Start Over'){
         RestartTimer();
       }
@@ -18,25 +22,33 @@
 
     function StartTime(){
       timer = $interval(function() {
-        $ctrl.state = 'Start Over';
-        $ctrl.time = $ctrl.time - 1;
-        if($ctrl.time == 0) {
-          $interval.cancel(timer);
-          $ctrl.state = 'Start';
-          $ctrl.time = 1500;
+          $ctrl.state = 'Start Over';
+
+          $ctrl.time = $ctrl.time - 1;
+          if($ctrl.time == 0 && !$ctrl.onBreak) {
+            $interval.cancel(timer);
+            $ctrl.state = 'Start';
+            $ctrl.time = 5;
+            $ctrl.onBreak = true;
           }
+          else if ($ctrl.time == 0 && $ctrl.onBreak){
+            $interval.cancel(timer);
+            $ctrl.state = 'Start';
+            $ctrl.time = 10;
+            $ctrl.onBreak = false;
+            }
       }, 1000);
     }
 
+
     function RestartTimer(){
       $interval.cancel(timer);
-      $ctrl.time = 1500;
+      $ctrl.onBreak = false;
+      $ctrl.time = 10;
       $ctrl.state = 'Start';
     }
-  }
-
+}
   angular
     .module('blocTime')
     .controller('LandingCtrl', LandingCtrl);
-
 })();
