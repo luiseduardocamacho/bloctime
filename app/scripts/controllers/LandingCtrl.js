a1d965da2c84ee8e45cbd6f350b74c525b16765f
Luis Camacho <luiscamacho@gmail.com>
2017-11-25 17:11:37 -0500

    Added filter to handle the conversion from a number to minutes and seconds

--- app/scripts/controllers/LandingCtrl.js
+++ app/scripts/controllers/LandingCtrl.js
@@ -1,7 +1,7 @@
 (function(){
   function LandingCtrl($interval){
     var $ctrl = this;
-    $ctrl.time = 10;
+    $ctrl.time = 15;
     $ctrl.StartPauseTime = StartPauseTime;
     $ctrl.StartTimer = StartTimer;
     $ctrl.state = 'Start';
@@ -24,12 +24,11 @@
     function StartTimer(){
       timer = $interval(function() {
           $ctrl.state = 'Start Over';
-
           $ctrl.time = $ctrl.time - 1;
           if($ctrl.time == 0 && !$ctrl.onBreak && $ctrl.breakCount < 4) {
             $interval.cancel(timer);
             $ctrl.state = 'Start';
-            $ctrl.time = 5;
+            $ctrl.time = 3;
             $ctrl.breakCount++;
             $ctrl.onBreak = true;
             console.log($ctrl.breakCount);
@@ -37,14 +36,14 @@
           else if($ctrl.time == 0 && !$ctrl.onBreak && $ctrl.breakCount == 4){
             $interval.cancel(timer);
             $ctrl.state = 'Start';
-            $ctrl.time = 30;
+            $ctrl.time = 18;
             $ctrl.breakCount = 1;
             $ctrl.onBreak = true;
           }
           else if ($ctrl.time == 0 && $ctrl.onBreak){
             $interval.cancel(timer);
             $ctrl.state = 'Start';
-            $ctrl.time = 10;
+            $ctrl.time = 15;
             $ctrl.onBreak = false;
             }
       }, 1000);
@@ -54,7 +53,7 @@
     function RestartTimer(){
       $interval.cancel(timer);
       $ctrl.onBreak = false;
-      $ctrl.time = 10;
+      $ctrl.time = 15;
       $ctrl.state = 'Start';
     }
 }
